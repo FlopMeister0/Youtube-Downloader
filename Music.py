5317ba7fa68e814157046d64028eff8924dc0e5f
FlopMeister0 <elijahpearson.010@gmail.com>
2024-12-26 12:55:01 +0000

    Version 1
    Can retrieve video ID

---
+++ Music.py
@@ -0,0 +1,46 @@
+# test link: https://www.youtube.com/watch?v=zLrOS5oz6IQ
+# streaming playlist: https://www.youtube.com/playlist?list=PLRvGeqCR1PHVpTBpuVQhd2oixW4FXi56z
+
+from pytube import YouTube
+from googleapiclient.discovery import build
+import googleapiclient 
+
+"""Api Connection"""
+api_version = "v3"
+api_service_name = "youtube"
+
+youtube_connection = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
+
+request = youtube_connection.playlistItems().list(
+   part = "contentDetails",
+   maxResults = "50",
+   playlistId = "PLRvGeqCR1PHVpTBpuVQhd2oixW4FXi56z"
+)
+response = request.execute() # response from the request
+
+"""Retrieves video ID"""
+def retrieve():
+    id = []
+    for item in response['items']:
+        id.append(response)
+        video_id = item['contentDetails']['videoId']
+        saving(video_id)
+        
+        print(video_id)
+
+############################################
+
+def saving(video_id):
+    Save_To = "/Users/elija/Desktop/Projects/Music/Music/MP3"
+    link = f"https://www.youtube.com/watch?v={video_id}"
+
+    yt = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+
+    mp3 = yt.streams.filter(file_extension="mp3").all()
+
+    try:
+        mp3.download(output_path=Save_To)
+    except:
+        print("error")
+
+retrieve()
