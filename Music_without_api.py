af991c421dfc92240660e562b1182078fbd65077
FlopMeister0 <elijahpearson.010@gmail.com>
2024-12-28 12:34:29 +0000

    MP4

--- Music_without_api.py
+++ Music_without_api.py
@@ -1,12 +1,13 @@
 # test link: https://www.youtube.com/watch?v=zLrOS5oz6IQ
 # streaming playlist: https://www.youtube.com/playlist?list=PLRvGeqCR1PHVpTBpuVQhd2oixW4FXi56z
+# It's Bangers and mash time: https://youtube.com/playlist?list=PLRvGeqCR1PHXu9gwYaYEU60s1sDuCe712&si=-5dnVHX5oledQjai
 
 from pytubefix import YouTube, Playlist
 from pytubefix.exceptions import VideoUnavailable
 import os
 
 """Retrieves video ID"""
-def retrieve():
+def MP3():
     Playlist_url = "https://www.youtube.com/playlist?list=PLRvGeqCR1PHVpTBpuVQhd2oixW4FXi56z"
     p = Playlist(Playlist_url)
     Save_To = "MP3"
@@ -22,5 +23,23 @@ def retrieve():
             base, extension = os.path.splitext(Download_File)
             mp3 = base + ".mp3"
             os.rename(Download_File, mp3)
+            print(Download_File)
 
-retrieve()
+# MP3()
+
+def MP4():
+    Playlist_url = "https://youtube.com/playlist?list=PLRvGeqCR1PHXu9gwYaYEU60s1sDuCe712&si=-5dnVHX5oledQjai"
+    p = Playlist(Playlist_url)
+    Save_To = "MP4"
+    
+    for url in p.video_urls:
+        try:
+            yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+        except VideoUnavailable:
+            print("Video Unavailable")
+            pass
+        else:
+            Download_File = yt.streams.first().download(output_path=Save_To)
+            print(Download_File)
+            
+MP4()
