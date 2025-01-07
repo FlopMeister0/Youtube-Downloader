9702eb45b712d8ea8cf3dc284c79ee880c2f52ff
FlopMeister <elijahpearson.010@gmail.com>
2025-01-07 12:54:54 +0000

    New Ui

--- YoutubeDownloader2.py
+++ YoutubeDownloader2.py
@@ -5,58 +5,43 @@ from pytubefix import YouTube, Playlist
 from pytubefix.exceptions import VideoUnavailable
 import urllib.request
 
-"""YoutubeDownloader Ui"""
+
 class Ui_YoutubeDownloader(object):
     def setupUi(self, YoutubeDownloader):
-        # Window
         YoutubeDownloader.setObjectName("YoutubeDownloader")
         YoutubeDownloader.resize(698, 378)
         YoutubeDownloader.setMinimumSize(QtCore.QSize(698, 378))
         YoutubeDownloader.setMaximumSize(QtCore.QSize(698, 378))
         self.centralwidget = QtWidgets.QWidget(YoutubeDownloader)
         self.centralwidget.setObjectName("centralwidget")
-        # Button Convert
         self.pushButtonConvert = QtWidgets.QPushButton(self.centralwidget, clicked = lambda: self.Convert())
         self.pushButtonConvert.setGeometry(QtCore.QRect(270, 40, 111, 31))
         self.pushButtonConvert.setObjectName("pushButtonConvert")
-        # List of downloaded items
         self.listDownloaded = QtWidgets.QListWidget(self.centralwidget)
         self.listDownloaded.setGeometry(QtCore.QRect(20, 130, 361, 191))
         self.listDownloaded.setObjectName("listDownloaded")
-        # Button Delete item
         self.pushButtonDelete = QtWidgets.QPushButton(self.centralwidget)
         self.pushButtonDelete.setGeometry(QtCore.QRect(20, 110, 361, 21))
         self.pushButtonDelete.setObjectName("pushButtonDelete")
-        # Text to edit link for video or playlist
         self.textEditLink = QtWidgets.QTextEdit(self.centralwidget)
         self.textEditLink.setGeometry(QtCore.QRect(20, 40, 251, 31))
         self.textEditLink.setObjectName("textEditLink")
-        # Name of video search query
         self.textEditName = QtWidgets.QTextEdit(self.centralwidget)
         self.textEditName.setGeometry(QtCore.QRect(20, 80, 251, 31))
         self.textEditName.setObjectName("textEditName")
-        # Progress bar
         self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
         self.progressBar.setGeometry(QtCore.QRect(20, 330, 361, 23))
         self.progressBar.setProperty("value", 24)
         self.progressBar.setObjectName("progressBar")
-        # Video only select button
         self.radioButtonVideo = QtWidgets.QRadioButton(self.centralwidget)
         self.radioButtonVideo.setGeometry(QtCore.QRect(390, 40, 82, 17))
         self.radioButtonVideo.setObjectName("radioButtonVideo")
-        # Audio only select button
         self.radioButtonAudio = QtWidgets.QRadioButton(self.centralwidget)
         self.radioButtonAudio.setGeometry(QtCore.QRect(390, 60, 82, 17))
         self.radioButtonAudio.setObjectName("radioButtonAudio")
-        # To be able to download the original source
         self.radioButtonVideoAndAudio = QtWidgets.QRadioButton(self.centralwidget)
         self.radioButtonVideoAndAudio.setGeometry(QtCore.QRect(390, 80, 91, 16))
         self.radioButtonVideoAndAudio.setObjectName("radioButtonVideoAndAudio")
-        # thumbnail download select button
-        self.radioButtonThumbnail = QtWidgets.QRadioButton(self.centralwidget)
-        self.radioButtonThumbnail.setGeometry(QtCore.QRect(390, 120, 171, 17))
-        self.radioButtonThumbnail.setObjectName("radioButtonThumbnail")
-        # textedit for settings format
         self.labelFormat = QtWidgets.QLabel(self.centralwidget)
         self.labelFormat.setGeometry(QtCore.QRect(390, 20, 131, 21))
         font = QtGui.QFont()
@@ -65,7 +50,6 @@ class Ui_YoutubeDownloader(object):
         font.setWeight(75)
         self.labelFormat.setFont(font)
         self.labelFormat.setObjectName("labelFormat")
-        # text for the link
         self.labelLink = QtWidgets.QLabel(self.centralwidget)
         self.labelLink.setGeometry(QtCore.QRect(20, 10, 121, 31))
         font = QtGui.QFont()
@@ -74,7 +58,6 @@ class Ui_YoutubeDownloader(object):
         font.setWeight(75)
         self.labelLink.setFont(font)
         self.labelLink.setObjectName("labelLink")
-        # text for the extra settings
         self.labelExtra = QtWidgets.QLabel(self.centralwidget)
         self.labelExtra.setGeometry(QtCore.QRect(390, 100, 131, 21))
         font = QtGui.QFont()
@@ -83,13 +66,11 @@ class Ui_YoutubeDownloader(object):
         font.setWeight(75)
         self.labelExtra.setFont(font)
         self.labelExtra.setObjectName("labelExtra")
-        # The edit line for where the user wants to download their items
         self.textEditDirectDownloads = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEditDirectDownloads.setGeometry(QtCore.QRect(390, 180, 291, 31))
+        self.textEditDirectDownloads.setGeometry(QtCore.QRect(390, 200, 291, 31))
         self.textEditDirectDownloads.setObjectName("textEditDirectDownloads")
-        # Text for directing user to the downloads text edit
         self.labelDirectDownloads = QtWidgets.QLabel(self.centralwidget)
-        self.labelDirectDownloads.setGeometry(QtCore.QRect(390, 140, 251, 41))
+        self.labelDirectDownloads.setGeometry(QtCore.QRect(390, 160, 251, 41))
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
@@ -97,11 +78,10 @@ class Ui_YoutubeDownloader(object):
         self.labelDirectDownloads.setFont(font)
         self.labelDirectDownloads.setWordWrap(True)
         self.labelDirectDownloads.setObjectName("labelDirectDownloads")
-        # Button to search the video query
         self.pushButtonSearch = QtWidgets.QPushButton(self.centralwidget)
         self.pushButtonSearch.setGeometry(QtCore.QRect(270, 80, 111, 31))
         self.pushButtonSearch.setObjectName("pushButtonSearch")
-        # Youtube Icon
+
         self.iconYoutube = QtWidgets.QLabel(self.centralwidget)
         self.iconYoutube.setGeometry(QtCore.QRect(560, 30, 121, 81))
         iconYoutube = "YouTube.png"
@@ -112,46 +92,46 @@ class Ui_YoutubeDownloader(object):
         self.iconYoutube.setPixmap(QtGui.QPixmap(icon_youtube_file_path))
         self.iconYoutube.setScaledContents(True)
         self.iconYoutube.setObjectName("iconYoutube")
-        # text for what playlist is selected
-        self.labelPlaylist = QtWidgets.QLabel(self.centralwidget)
-        self.labelPlaylist.setGeometry(QtCore.QRect(390, 230, 141, 41))
+
+        self.labelPlaylistSelected = QtWidgets.QLabel(self.centralwidget)
+        self.labelPlaylistSelected.setGeometry(QtCore.QRect(390, 240, 141, 41))
         font = QtGui.QFont()
         font.setPointSize(11)
         font.setBold(True)
         font.setWeight(75)
-        self.labelPlaylist.setFont(font)
-        self.labelPlaylist.setObjectName("labelPlaylist")
-        # text for what video is selected
-        self.labelVideo = QtWidgets.QLabel(self.centralwidget)
-        self.labelVideo.setGeometry(QtCore.QRect(390, 290, 141, 41))
+        self.labelPlaylistSelected.setFont(font)
+        self.labelPlaylistSelected.setObjectName("labelPlaylistSelected")
+        self.labelVideoSelected = QtWidgets.QLabel(self.centralwidget)
+        self.labelVideoSelected.setGeometry(QtCore.QRect(390, 290, 141, 41))
         font = QtGui.QFont()
         font.setPointSize(11)
         font.setBold(True)
         font.setWeight(75)
-        self.labelVideo.setFont(font)
-        self.labelVideo.setObjectName("labelVideo")
-        # The Textedit for the selected playlist 
+        self.labelVideoSelected.setFont(font)
+        self.labelVideoSelected.setObjectName("labelVideoSelected")
         self.textEditPlaylistSelected = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEditPlaylistSelected.setGeometry(QtCore.QRect(390, 260, 291, 31))
+        self.textEditPlaylistSelected.setGeometry(QtCore.QRect(390, 270, 291, 31))
         self.textEditPlaylistSelected.setObjectName("textEditPlaylistSelected")
-        # The Textedit for the selected video
         self.textEditVideoSelected = QtWidgets.QTextEdit(self.centralwidget)
         self.textEditVideoSelected.setGeometry(QtCore.QRect(390, 320, 291, 31))
         self.textEditVideoSelected.setObjectName("textEditVideoSelected")
-        # The line seperating the settings and selection
         self.line = QtWidgets.QFrame(self.centralwidget)
-        self.line.setGeometry(QtCore.QRect(380, 230, 321, 20))
+        self.line.setGeometry(QtCore.QRect(380, 240, 321, 20))
         self.line.setFrameShape(QtWidgets.QFrame.HLine)
         self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line.setObjectName("line")
-        # The button confirming the download directory
         self.pushButtonConfirm = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButtonConfirm.setGeometry(QtCore.QRect(390, 210, 141, 21))
+        self.pushButtonConfirm.setGeometry(QtCore.QRect(390, 230, 141, 21))
         self.pushButtonConfirm.setObjectName("pushButtonConfirm")
-        # The button to set download directory to default
         self.pushButtonDefault = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButtonDefault.setGeometry(QtCore.QRect(530, 210, 151, 21))
+        self.pushButtonDefault.setGeometry(QtCore.QRect(530, 230, 151, 21))
         self.pushButtonDefault.setObjectName("pushButtonDefault")
+        self.checkBoxThumbnail = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBoxThumbnail.setGeometry(QtCore.QRect(390, 120, 151, 17))
+        self.checkBoxThumbnail.setObjectName("checkBoxThumbnail")
+        self.checkBoxPlaylist = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBoxPlaylist.setGeometry(QtCore.QRect(390, 140, 211, 17))
+        self.checkBoxPlaylist.setObjectName("checkBoxPlaylist")
         YoutubeDownloader.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(YoutubeDownloader)
         self.statusbar.setObjectName("statusbar")
@@ -160,7 +140,6 @@ class Ui_YoutubeDownloader(object):
         self.retranslateUi(YoutubeDownloader)
         QtCore.QMetaObject.connectSlotsByName(YoutubeDownloader)
 
-    """Translates text to widgets"""
     def retranslateUi(self, YoutubeDownloader):
         _translate = QtCore.QCoreApplication.translate
         YoutubeDownloader.setWindowTitle(_translate("YoutubeDownloader", "MainWindow"))
@@ -169,28 +148,29 @@ class Ui_YoutubeDownloader(object):
         self.radioButtonVideo.setText(_translate("YoutubeDownloader", "Video only"))
         self.radioButtonAudio.setText(_translate("YoutubeDownloader", "Audio only"))
         self.radioButtonVideoAndAudio.setText(_translate("YoutubeDownloader", "Video + Audio"))
-        self.radioButtonThumbnail.setText(_translate("YoutubeDownloader", "Download  Thumbnail as well"))
         self.labelFormat.setText(_translate("YoutubeDownloader", "Format Settings"))
-        self.labelLink.setText(_translate("YoutubeDownloader", "Input Link Here"))
+        self.labelLink.setText(_translate("YoutubeDownloader", "Input Link Here:"))
         self.labelExtra.setText(_translate("YoutubeDownloader", "Extra Settings"))
         self.labelDirectDownloads.setText(_translate("YoutubeDownloader", "Where should the downloads go? (leave blank for application folder)"))
         self.pushButtonSearch.setText(_translate("YoutubeDownloader", "Search"))
-        self.labelPlaylist.setText(_translate("YoutubeDownloader", "Playlist Selected:"))
-        self.labelVideo.setText(_translate("YoutubeDownloader", "Video Selected:"))
+        self.labelPlaylistSelected.setText(_translate("YoutubeDownloader", "Playlist Selected:"))
+        self.labelVideoSelected.setText(_translate("YoutubeDownloader", "Video Selected:"))
         self.pushButtonConfirm.setText(_translate("YoutubeDownloader", "Confirm"))
         self.pushButtonDefault.setText(_translate("YoutubeDownloader", "Default"))
+        self.checkBoxThumbnail.setText(_translate("YoutubeDownloader", "Download Thumbnail Aswell"))
+        self.checkBoxPlaylist.setText(_translate("YoutubeDownloader", "Check for Playlist / Uncheck for Video"))
 
     def Convert(self):
         """Functions"""
         # if the user wants a thumbnail to be downloaded also
-        thumbnail_flag = self.radioButtonThumbnail.isChecked()
+        thumbnail_flag = self.checkBoxThumbnail.isChecked()
         # playlist url
-        Playlist_url = self.textEditPlaylistSelected.toPlainText()
+        Playlist_url = self.textEditLink.toPlainText()
         # where it will be saved
 
-        Save_To = self.textEditDirectDownloads.toPlainText()
+        Save_To = self.textEditLink.toPlainText()
         if not Save_To:
-            if Save_To == "" and self.radioButtonAudio.isChecked():
+            if Save_To == "" and self.radioButtonAudio.isChecked() and self.checkBoxPlaylist.isChecked():
                 Save_To = "Audio"
                 DownloadAudio(thumbnail_flag, Playlist_url, Save_To)
             elif Save_To == "" and self.radioButtonVideo.isChecked():
@@ -202,30 +182,8 @@ class Ui_YoutubeDownloader(object):
         if not os.path.isdir(Save_To):
             self.textEditDirectDownloads.setText("invalid directory!")
             
-
-
-
-
-
-
-"""Retrieves Original Video"""
-def Original():
-    Playlist_url = "https://youtube.com/playlist?list=PLRvGeqCR1PHXu9gwYaYEU60s1sDuCe712&si=-5dnVHX5oledQjai"
-    p = Playlist(Playlist_url)
-    Save_To = "Video"
-    
-    for url in p.video_urls:
-        try:
-            yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
-        except VideoUnavailable:
-            print("Video Unavailable")
-            pass
-        else:
-            Download_File = yt.streams.first().download(output_path=Save_To)
-            print(Download_File)
-
-"""Downloads Video Only"""
-def DownloadVideo(Playlist_url, Save_To, thumbnail_flag):
+"""Downloads Audio"""
+def DownloadAudio(Playlist_url, Save_To, thumbnail_flag):
     
     print("Download")
 
@@ -254,8 +212,28 @@ def DownloadVideo(Playlist_url, Save_To, thumbnail_flag):
             if thumbnail_flag == True: # if the flag is set to true
                 DownloadThumbnail(base, url)
 
-"""Downloads Audio"""
-def DownloadAudio(Playlist_url, Save_To, thumbnail_flag):
+
+
+
+
+"""Retrieves Original Video"""
+def Original():
+    Playlist_url = "https://youtube.com/playlist?list=PLRvGeqCR1PHXu9gwYaYEU60s1sDuCe712&si=-5dnVHX5oledQjai"
+    p = Playlist(Playlist_url)
+    Save_To = "Video"
+    
+    for url in p.video_urls:
+        try:
+            yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+        except VideoUnavailable:
+            print("Video Unavailable")
+            pass
+        else:
+            Download_File = yt.streams.first().download(output_path=Save_To)
+            print(Download_File)
+
+"""Downloads Video Only"""
+def DownloadVideo(Playlist_url, Save_To, thumbnail_flag):
     
     print("Download")
 
